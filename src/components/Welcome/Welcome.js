34d8d4b8171df1196dc960849a0632188d436af7
andreglegg <andreglegg@me.com>
2018-04-21 16:56:44 +0200

    Ejected

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import classes from './Welcome.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className={classes.Welcome}>
+                Welcome
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Welcome from '../../components/Welcome/Welcome';
+
+class Home extends Component {
+    render() {
+        return (
+            <Welcome />
+        );
+    }
+}
+
+
+export default Home;
